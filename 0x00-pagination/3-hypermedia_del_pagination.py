5a2768d873177dd78fe3c873abb691e997725d69
mainbosskay <dadakehinde80@yahoo.com>
2024-08-17 14:37:31 +0000

    Python program that Implement a get_hyper_index method with two integer arguments: index with a None default value and page_size with default value of 10

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Deletion-resilient hypermedia pagination"""
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initializing instance of server"""
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset for csv file"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Getting page info from specific index with given size"""
+        dataId = self.indexed_dataset()
+        assert index is not None and index >= 0 and index <= max(dataId.keys())
+        data = []
+        dataCount = 0
+        next_index = None
+        startIndx = index if index else 0
+        for k, element in dataId.items():
+            if k >= startIndx and dataCount < page_size:
+                data.append(element)
+                dataCount += 1
+                continue
+            if dataCount == page_size:
+                next_index = k
+                break
+        hyperMediaPageInfo = {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data
+        }
+        return hyperMediaPageInfo
