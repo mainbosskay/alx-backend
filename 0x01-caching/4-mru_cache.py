47bcba303f94caa81fa0353c1321e9db27b6c3d0
mainbosskay <dadakehinde80@yahoo.com>
2024-08-21 08:38:36 +0000

    Creating class MRUCache that inherits from BaseCaching and is a caching system

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -20,12 +20,12 @@ class MRUCache(BaseCaching):
                 mruKey, _ = self.cache_data.popitem(False)
                 print("DISCARD:", mruKey)
             self.cache_data[key] = item
-            self.cache_data.move_tothe_end(key, last=False)
+            self.cache_data.move_to_end(key, last=False)
         else:
             self.cache_data[key] = item
 
     def get(self, key):
         """Getting an item from cache using specific key"""
         if key is not None and key in self.cache_data:
-            self.cache_data.move_tothe_end(key, last=False)
+            self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
